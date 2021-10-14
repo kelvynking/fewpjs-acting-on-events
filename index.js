82cfe993965cdc3746f256c14e93455266b646cf
kelvynking <kelvyn.m.king@gmail.com>
2021-10-14 14:09:05 +1100

    Created function 'movedodgerRight' which moves the DODGER to the right.

--- index.js
+++ index.js
@@ -10,8 +10,24 @@ function moveDodgerLeft() {
     }
 }
 
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+
+    let left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+    }
+}
+
 document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
         moveDodgerLeft();
     }
 });
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
